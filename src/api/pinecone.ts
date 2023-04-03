40f3f2fd1224c73f746d69f4c324492899d524d3
Dustin Lacewell <dlacewell@gmail.com>
2023-04-03 15:06:29 -0500

    Improve pinecone chunking support

--- src/api/pinecone.ts
+++ src/api/pinecone.ts
@@ -1,90 +1,83 @@
 import { PineconeClient } from "@pinecone-database/pinecone"
 import { getPineconeIndex } from "../environment/getPineconeIndex"
+import { getPineconeEnvironment } from "../environment/getPineconeEnvironment"
+import { getPineconeKey } from "../environment/getPineconeKey"
+import { embedChunks } from "./openai"
+import { Chunk, chunkFile, chunkFolder } from "../utils/chunking"
+import { createUUID } from "../utils/uuid"
 
-const createUUID = () => {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-        var r = (Math.random() * 16) | 0,
-            v = c === "x" ? r : (r & 0x3) | 0x8
-        return v.toString(16)
-    })
-}
 
 const createClient = async () => {
     const pinecone = new PineconeClient()
 
     await pinecone.init({
-        environment: "YOUR_ENVIRONMENT",
-        apiKey: "YOUR_API_KEY",
+        environment: getPineconeEnvironment()!,
+        apiKey: getPineconeKey()!,
     })
 
     return pinecone
 }
 
-const upsert = async (values: number[], metadata: any, namespace = "") => {
-    const id = createUUID()
+const getIndex = async () => {
     const pinecone = await createClient()
     const indexName = getPineconeIndex()
-    const index = pinecone.Index(indexName!)
+    return pinecone.Index(indexName!)
+}
+
+const convertChunk = (chunk: Chunk, metadata = {}) => ({
+    id: createUUID(),
+    values: chunk.embedding!,
+    metadata: {
+        ...metadata,
+        filename: chunk.filename,
+        start: chunk.start,
+        end: chunk.end,
+        text: chunk.text,
+    },
+})
+
+const upsertChunks = async (chunks: Chunk[], namespace = "", metadata = {}) => {
+    const index = await getIndex()
+    const vectors = chunks.map(convertChunk, metadata)
     const upsertRequest = {
-        vectors: [{
-            id,
-            values,
-            metadata
-        }],
+        vectors,
         namespace,
     }
-
-    return index.upsert({ upsertRequest })
+    await index.upsert({ upsertRequest })
 }
 
-// chunk(file, block_size, block_overlap) splits a file into chunks of size block_size, with block_overlap overlap between chunks.
-// The last chunk may be smaller than block_size.
-const chunk = (file: string, block_size: number, block_overlap: number) => {
-    const chunks = []
-    let start = 0
-    let end = block_size
-    let overlap = 0
-
-    while (start < file.length) {
-        if (end > file.length) {
-            end = file.length
-        }
-
-        chunks.push(file.substring(start, end))
-
-        start = end - overlap // set start to the end of the previous chunk, minus the overlap
-        end = start + block_size // set end to the end of the new chunk
-    }
-
-    return chunks
+export const upsertFile = async (filename: string, block_size: number, block_overlap: number, metadata: any, namespace = "") => {
+    const chunks = chunkFile(filename, block_size, block_overlap)
+    await embedChunks(chunks)
+    await upsertChunks(chunks, namespace, metadata)
 }
 
-// upsettFile takes a file and splits it into chunks of size block_size, with block_overlap overlap between chunks.
-export const upsertFile = async (file: string, block_size: number, block_overlap: number, metadata: any, namespace = "") => {
-    const chunks = chunk(file, block_size, block_overlap)
 
-    for (let i = 0; i < chunks.length; i++) {
-        const values = chunks[i].split("").map((c) => c.charCodeAt(0))
-        const _metadata = {
-            ...metadata,
-            filename: file,
-            chunk: i,
-        }
-        await upsert(values, _metadata, namespace)
-    }
+export const upsertFolder = async (folder: string, block_size: number, block_overlap: number, metadata: any, namespace = "") => {
+    const chunks = chunkFolder(folder, block_size, block_overlap)
+    await embedChunks(chunks)
+    await upsertChunks(chunks, namespace, metadata)
 }
 
-// query takes a query vector and returns the top k results.
-const query = async (queryVector: number[], topK: number, filter = {}, namespace = "") => {
+export const query = async (vector: number[], topK: number, filter = undefined, namespace = "") => {
     const client = await createClient()
     const indexName = getPineconeIndex()
     const index = client.Index(indexName!)
     const queryRequest = {
-        queryVector,
+        vector,
         topK,
         filter,
         namespace,
+        includeMetadata: true,
+        includeValues: false,
     }
 
     return index.query({ queryRequest })
+}
+
+export const clearNamespace = async (namespace = "") => {
+    const client = await createClient()
+    const indexName = getPineconeIndex()
+    const index = client.Index(indexName!)
+    return index.delete1({ deleteAll : true, namespace })
 }
