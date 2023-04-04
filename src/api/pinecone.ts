fae1a031282efbefd421a4978b4fce198c1378f9
o <poteat@users.noreply.github.com>
2023-04-03 19:35:46 -0700

    Format some pinecone files via Prettier extension

--- src/api/pinecone.ts
+++ src/api/pinecone.ts
@@ -6,92 +6,105 @@ import { embedChunks } from "./openai"
 import { Chunk, chunkFile, chunkFolder } from "../utils/chunking"
 import { createUUID } from "../utils/uuid"
 
-
 const createClient = async () => {
-    const pinecone = new PineconeClient()
+  const pinecone = new PineconeClient()
 
-    await pinecone.init({
-        environment: getPineconeEnvironment()!,
-        apiKey: getPineconeKey()!,
-    })
+  await pinecone.init({
+    environment: getPineconeEnvironment()!,
+    apiKey: getPineconeKey()!,
+  })
 
-    return pinecone
+  return pinecone
 }
 
 const getIndex = async () => {
-    const pinecone = await createClient()
-    const indexName = getPineconeIndex()
-    return pinecone.Index(indexName!)
+  const pinecone = await createClient()
+  const indexName = getPineconeIndex()
+  return pinecone.Index(indexName!)
 }
 
 const convertChunk = (chunk: Chunk, metadata = {}) => ({
-    id: createUUID(),
-    values: chunk.embedding!,
-    metadata: {
-        ...metadata,
-        filename: chunk.filename,
-        start: chunk.start,
-        end: chunk.end,
-        text: chunk.text,
-    },
+  id: createUUID(),
+  values: chunk.embedding!,
+  metadata: {
+    ...metadata,
+    filename: chunk.filename,
+    start: chunk.start,
+    end: chunk.end,
+    text: chunk.text,
+  },
 })
 
 const upsertChunks = async (chunks: Chunk[], namespace = "", metadata = {}) => {
-    const index = await getIndex()
-    const vectors = chunks.map(convertChunk, metadata)
-    console.log(`Upserting chunks:`)
-    for (let i = 0; i < vectors.length; i += 100) {
-        console.log(`  ${i} - ${i + 100} of ${vectors.length}`)
-        const upsertRequest = {
-            vectors: vectors.slice(i, i + 100),
-            namespace,
-        }
-        await index.upsert({ upsertRequest })
+  const index = await getIndex()
+  const vectors = chunks.map(convertChunk, metadata)
+  console.log(`Upserting chunks:`)
+  for (let i = 0; i < vectors.length; i += 100) {
+    console.log(`  ${i} - ${i + 100} of ${vectors.length}`)
+    const upsertRequest = {
+      vectors: vectors.slice(i, i + 100),
+      namespace,
     }
+    await index.upsert({ upsertRequest })
+  }
 }
 
-export const upsertFile = async (filename: string, block_size: number, block_overlap: number, metadata: any, namespace = "") => {
-    const chunks = chunkFile(filename, block_size, block_overlap)
-    await embedChunks(chunks)
-    await upsertChunks(chunks, namespace, metadata)
+export const upsertFile = async (
+  filename: string,
+  block_size: number,
+  block_overlap: number,
+  metadata: any,
+  namespace = ""
+) => {
+  const chunks = chunkFile(filename, block_size, block_overlap)
+  await embedChunks(chunks)
+  await upsertChunks(chunks, namespace, metadata)
 }
 
-
-export const upsertFolder = async (folder: string, block_size: number, block_overlap: number, metadata: any, namespace = "") => {
-    const chunks = chunkFolder(folder, block_size, block_overlap)
-    await embedChunks(chunks)
-    await upsertChunks(chunks, namespace, metadata)
+export const upsertFolder = async (
+  folder: string,
+  block_size: number,
+  block_overlap: number,
+  metadata: any,
+  namespace = ""
+) => {
+  const chunks = chunkFolder(folder, block_size, block_overlap)
+  await embedChunks(chunks)
+  await upsertChunks(chunks, namespace, metadata)
 }
 
-export const query = async (vector: number[], topK: number, filter = undefined, namespace = "") => {
-    const client = await createClient()
-    const indexName = getPineconeIndex()
-    const index = client.Index(indexName!)
-    const queryRequest = {
-        vector,
-        topK,
-        filter,
-        namespace,
-        includeMetadata: true,
-        includeValues: false,
-    }
+export const query = async (
+  vector: number[],
+  topK: number,
+  filter = undefined,
+  namespace = ""
+) => {
+  const client = await createClient()
+  const indexName = getPineconeIndex()
+  const index = client.Index(indexName!)
+  const queryRequest = {
+    vector,
+    topK,
+    filter,
+    namespace,
+    includeMetadata: true,
+    includeValues: false,
+  }
 
-    return index.query({ queryRequest })
+  return index.query({ queryRequest })
 }
 
 export const clearNamespace = async (namespace = "") => {
-    const client = await createClient()
-    const indexName = getPineconeIndex()
-    const index = client.Index(indexName!)
-    return index.delete1({ deleteAll : true, namespace })
+  const client = await createClient()
+  const indexName = getPineconeIndex()
+  const index = client.Index(indexName!)
+  return index.delete1({ deleteAll: true, namespace })
 }
 
 // run DescribeIndexStats
 export const indexStats = async () => {
-    const index = await getIndex()
-    return index.describeIndexStats({
-        describeIndexStatsRequest: {
-            
-        }
-    })
+  const index = await getIndex()
+  return index.describeIndexStats({
+    describeIndexStatsRequest: {},
+  })
 }
--- src/commands/indexWorkspace.ts
+++ src/commands/indexWorkspace.ts
@@ -2,25 +2,18 @@ import * as vscode from "vscode"
 import { clearNamespace, upsertFolder } from "../api/pinecone"
 import { makeAbsolute } from "../utils/paths"
 
-
 export const indexWorkspace = async () => {
-    try {
-        await clearNamespace("code")
-        vscode.window.showInformationMessage("Indexing workspace...")
+  try {
+    await clearNamespace("code")
+    vscode.window.showInformationMessage("Indexing workspace...")
 
-        await upsertFolder(
-            makeAbsolute("src"),
-            100,
-            10,
-            {},
-            "code"
-        )
-        vscode.window.showInformationMessage("Workspace indexed!")
-    } catch (error) {
-        console.error(error)
-        vscode.window.showErrorMessage("Error indexing workspace")
-        if (error instanceof Error) {
-            vscode.window.showErrorMessage(error.message)
-        }
+    await upsertFolder(makeAbsolute("src"), 100, 10, {}, "code")
+    vscode.window.showInformationMessage("Workspace indexed!")
+  } catch (error) {
+    console.error(error)
+    vscode.window.showErrorMessage("Error indexing workspace")
+    if (error instanceof Error) {
+      vscode.window.showErrorMessage(error.message)
     }
+  }
 }
