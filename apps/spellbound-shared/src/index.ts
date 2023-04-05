0859de9bc19f472f2d0525dd53eb71c893887bf0
Dustin Lacewell <dlacewell@gmail.com>
2023-04-04 23:56:54 -0500

    Remove mention of tRPC

--- apps/spellbound-shared/src/index.ts
+++ apps/spellbound-shared/src/index.ts
@@ -1,5 +1,2 @@
 export * from './birpc';
-export * from './client';
-export * from './rpc';
-export * from './server';
 export * from './types';
--- apps/spellbound-shared/src/rpc/server/index.ts
+++
@@ -1,5 +0,0 @@
-export * from '../MessagePipe';
-export * from './Server';
-export * from './SubscriptionManager';
-export * from './types';
-export * from './utils';
