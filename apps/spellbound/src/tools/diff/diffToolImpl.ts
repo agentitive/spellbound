6b3194ad1f5a7eb87db4010f78d30f73ec97b212
Dustin Lacewell <dlacewell@gmail.com>
2023-04-06 00:33:27 -0500

    Stringify diff results for now, because I think the typings are wrong

--- apps/spellbound/src/tools/diff/diffToolImpl.ts
+++ apps/spellbound/src/tools/diff/diffToolImpl.ts
@@ -14,7 +14,7 @@ export async function diffToolImpl(params: DiffToolInterface): Promise<string> {
     const filename = path.join(workspaceRoot || "", source)
     const text = fs.readFileSync(filename, "utf8")
     const patchedText = applyPatch(text, patches[0])
-    fs.writeFileSync(filename, patchedText)
+    fs.writeFileSync(filename, JSON.stringify(patchedText))
     return patchedText
   } catch (err: any) {
     return `ERROR: Failed to apply diff patch: ${err?.message || 'Unknown error'}`
