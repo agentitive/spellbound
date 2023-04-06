3dadb4f0a4a91eef707ccf2693a336dd779a4403
Dustin Lacewell <dlacewell@gmail.com>
2023-04-06 00:47:45 -0500

    Export grep tool

--- apps/spellbound/src/tools/AnyToolInterface.ts
+++ apps/spellbound/src/tools/AnyToolInterface.ts
@@ -2,19 +2,19 @@ import { CatToolInterface } from "./cat/CatToolInterface"
 import { LsToolInterface } from "./ls/LsToolInterface"
 import { SearchToolInterface } from "./search/SearchToolInterface"
 import { WriteToolInterface } from "./write/WriteToolInterface"
-import { ReplaceToolInterface } from "./replace/ReplaceToolInterface"
 import { AskToolInterface } from "./ask/AskToolInterface"
 import { NpmToolInterface } from "./npm/NpmToolInterface"
 import { DoneToolInterface } from "./done/DoneToolInterface"
 import { StatsToolInterface } from "./stats/StatsToolInterface"
 import { GitToolInterface } from "./git/GitToolInterface"
+import { GrepToolInterface } from "./grep/GrepToolInterface"
 
 export type AnyToolInterface =
   | CatToolInterface
   | LsToolInterface
   | SearchToolInterface
   | WriteToolInterface
-  | ReplaceToolInterface
+  | GrepToolInterface
   | AskToolInterface
   | NpmToolInterface
   | GitToolInterface
--- apps/spellbound/src/tools/grep/grepToolImpl.ts
+++ apps/spellbound/src/tools/grep/grepToolImpl.ts
@@ -1,8 +1,10 @@
-import { GrepToolInterface } from './GrepToolInterface';
-import grob from 'grob';
+import { GrepToolInterface } from './GrepToolInterface'
+
+// @ts-ignore
+import grob from 'grob'
 
 export async function grepToolImpl(params: GrepToolInterface) {
-  const { globs, regex } = params;
-  const res = await grob({ cwd: process.cwd(), globs, regex });
-  return res;
+  const { globs, regex } = params
+  const res = await grob({ cwd: process.cwd(), globs, regex })
+  return res
 }
