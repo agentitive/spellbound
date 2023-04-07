8c68a7035b6760a19d366891353956153e39df71
Dustin Lacewell <dlacewell@gmail.com>
2023-04-07 18:00:53 -0500

    remove unused openai refactor

--- apps/spellbound/src/openai/embeddings/embedChunk.ts
+++
@@ -1,7 +0,0 @@
-
-import { Chunk } from "../../utils/chunking"
-import { getEmbedding } from "./getEmbedding"
-
-export const embedChunk = async (chunk: Chunk) => {
-  chunk.embedding = await getEmbedding(chunk.text)
-}
--- apps/spellbound/src/openai/embeddings/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './embedChunk'
-export * from './embedChunks'
-export * from './getEmbedding'
-export * from './getEmbeddings'
